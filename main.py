dd32b34f482943e6d3cc474f444e21ed8c96db41
MaximeMalpica <m.malpicaarana@students.ephec.be>
2023-12-09 20:09:11 +0100

    Ajout des méthodes
    - Ajout des méthodes de la class Fraction

--- main.py
+++ main.py
@@ -63,3 +63,146 @@ class Fraction:
         if not isinstance(num, Fraction):
             raise ValueError("Doit être un int ou une fraction")
         return num
+
+# ------------------ Textual representations ------------------
+
+    def __str__(self) :
+        """Return a textual representation of the reduced form of the fraction
+
+        POST : Renvoie un string contenant le numérateur et le dénominateur séparés par un '/'gg
+        """
+        return f'{self.__numerator}/{self.__denominator}'
+
+    def as_mixed_number(self):
+        """Return a textual representation of the reduced form of the fraction as a mixed number.
+        A mixed number is the sum of an integer and a proper fraction
+
+        POST : Renvoie un string avec la valeur int de la fraction et le reste de la fraction séparé par un '+'.
+        """
+
+        int_value = self.numerator // self.denominator
+        fraction_value = Fraction(self.numerator % self.denominator, self.denominator)
+        return f'{int_value} + {fraction_value}'
+
+   # ------------------ Operators overloading ------------------
+
+    def __add__(self, other):
+        """Overloading of the + operator for fractions.
+
+        PRE : 'other' est une instance de Fraction
+        POST : Renvoie une nouvelle fraction représentant la somme des deux fractions
+        """
+        other = self.is_fraction(other)
+        add_num = self.numerator * other.denominator + self.denominator * other.numerator
+        add_den = self.denominator * other.denominator
+        return Fraction(add_num, add_den)
+
+    def __sub__(self, other):
+        """Overloading of the - operator for fractions.
+
+        PRE: 'other' est une instance de Fraction
+        POST: Renvoie une nouvelle fraction représentant la différence des deux fractions
+        """
+        other = self.is_fraction(other)
+        sub_num = self.numerator * other.denominator - self.denominator * other.numerator
+        sub_den = self.denominator * other.denominator
+        return Fraction(sub_num, sub_den)
+
+    def __mul__(self, other):
+        """Overloading of the * operator for fractions.
+
+        PRE: 'other' est une instance de Fraction
+        POST: Renvoie une nouvelle fraction représentant la multiplication des deux fractions
+        """
+        other = self.is_fraction(other)
+        mul_num = self.numerator * other.numerator
+        mul_den = self.denominator * other.denominator
+        return Fraction(mul_num, mul_den)
+
+    def __truediv__(self, other :int):
+        """Overloading of the / operator for fractions.
+
+        PRE : 'other' est une instance de Fraction et 'other' n'est pas une fraction nulle
+        POST : Renvoie une nouvelle fraction représentant la division des deux fractions
+        RAISES : ZeroDivisionError si 'den' est nul
+        """
+
+        if other.numerator == 0:
+            raise ZeroDivisionError("La division par zéro n'est pas autorisée.")
+        other = self.is_fraction(other)
+        div_num = self.numerator * other.denominator
+        div_den = self.denominator * other.numerator
+        return Fraction(div_num, div_den)
+
+    def __pow__(self, other: int):
+        """Overloading of the ** operator for fractions.
+
+        PRE: 'other ' est un entier
+        POST: Renvoie une nouvelle fraction représentant la puissance de la fraction
+        RAISES : TypeError si other n'est pas un nombre entier
+        """
+
+        if not isinstance(other, int):
+            raise TypeError('Doit être un nombre entier')
+
+        pow_num = self.numerator ** other
+        pow_den = self.denominator ** other
+        return Fraction(pow_num, pow_den)
+    
+    def __eq__(self, other:int) -> bool:
+        """Overloading of the == operator for fractions.
+
+        PRE: 'other' est une instance de Fraction
+        POST: Renvoie True si les deux fractions sont équivalentes , False sinon
+        """
+        other = self.is_fraction(other)
+        return self.numerator == other.numerator and self.denominator == other.denominator
+
+    def __float__(self) -> float:
+        """Returns the decimal value of the fraction.
+
+        POST: Renvoie la valeur décimale de la fraction
+        """
+        return self.numerator / self.denominator
+    
+# TODO : [BONUS] You can overload other operators if you wish (ex : <, >, ...)
+
+    # ------------------ Properties checking  ------------------
+
+    def is_zero(self):
+        """Check if a fraction's value is 0.
+
+        POST : Renvoie True si la fraction est nulle, False sinon
+        """
+        return not self.numerator
+
+    def is_integer(self):
+        """Check if a fraction is integer (ex : 8/4, 3, 2/2, ...).
+
+        POST : Renvoie True si la fraction est un entier, False sinon
+        """
+        return self.denominator == 1
+
+    def is_proper(self):
+        """Check if the absolute value of the fraction is < 1.
+
+        POST : Renvoie True si la fraction est propre, False sinon
+        """
+        return abs(float(self)) < 1
+
+    def is_unit(self):
+        """Check if a fraction's numerator is 1 in its reduced form.
+
+        POST : Renvoie True si le numérateur est 1, False sinon
+        """
+        return self.numerator == 1
+
+    def is_adjacent_to(self, other) :
+        """Check if two fractions differ by a unit fraction
+        Two fractions are adjacents if the absolute value of the difference them is a unit fraction.
+
+        PRE : Une fraction.
+        POST : Renvoie True si deux fractions sont adjacentes sinon False.
+        """
+        diff = abs(self.numerator * other.denominator - other.numerator * self.denominator)
+        return diff == 1
